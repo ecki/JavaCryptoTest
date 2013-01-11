1be618ea3074b8c6f9372409380d31f2172eb7f5
Bernd Eckenfels <bernd@eckenfels.net>
2013-01-11 02:23:40 +0100

    Some JCE tests (dump all Providers/Services and do some performance tests with two windows PRNG algorithms)

---
+++ src/main/java/net/eckenfels/test/jce/JCEProviderInfo.java
@@ -0,0 +1,82 @@
+package net.eckenfels.test.jce;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Provider;
+import java.security.Provider.Service;
+import java.security.SecureRandom;
+import java.security.Security;
+
+import sun.security.provider.NativePRNG;
+
+public class JCEProviderInfo
+{
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        //System.setProperty("java.security.egd", "test");
+
+        Provider[] ps = Security.getProviders();
+        for(Provider p : ps)
+        {
+            System.out.println("--- " + p.getName() + " " + p.getVersion() + " " + p.getInfo());
+            for(Service s : p.getServices())
+                //if (s.getType().equals("SecureRandom"))
+                    System.out.println("  " + s.getType() + "." + s.getAlgorithm() +  " : " + s.getClassName() + " (" + s.getProvider().getName() + ") " + s.toString());
+        }
+
+        SecureRandom s = new SecureRandom();
+        System.out.println("Default      " + s.getAlgorithm() + " " + s.getProvider().getName());
+
+
+        // speed compare
+
+        s = SecureRandom.getInstance("Windows-PRNG");
+        long start = System.currentTimeMillis();
+        s.nextBoolean();
+        long end = System.currentTimeMillis();
+        System.out.println("Windows-PRNG " + s.getAlgorithm() + " " + s.getProvider().getName() + " : " + s.getClass().getCanonicalName() + " Seeded in " + (end - start) + "ms");
+
+
+        start = System.currentTimeMillis();
+        for(int i=0;i<10000;i++)
+        {
+            s = SecureRandom.getInstance("Windows-PRNG");
+            s.nextInt();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("Windows-PRNG 10000*int Took " + (end - start) + "ms with instantiation/seeding");
+
+        start = System.currentTimeMillis();
+        for(int i=0;i<10000;i++)
+        {
+            s.nextInt();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("Windows-PRNG 10000*int Took " + (end - start) + "ms without instantiation/seeding");
+
+
+        s = SecureRandom.getInstance("SHA1PRNG");
+        start = System.currentTimeMillis();
+        s.nextBoolean();
+        end = System.currentTimeMillis();
+        System.out.println("SHA1PRNG " + s.getAlgorithm() + " " + s.getProvider().getName() + " : " + s.getClass().getCanonicalName() + " Seeded in " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+        for(int i=0;i<10000;i++)
+        {
+            s = SecureRandom.getInstance("SHA1PRNG");
+            s.nextInt();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("SHA1PRNG 10000*int Took " + (end - start) + "ms with instantiation/seeding");
+
+        start = System.currentTimeMillis();
+        for(int i=0;i<10000;i++)
+        {
+            s.nextInt();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("SHA1PRNG 10000*int Took " + (end - start) + "ms without instantiation/seeding");
+    }
+
+}
---
+++ src/main/java/net/eckenfels/test/jce/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package to test some aspects of Java Cryptography API/Extension.
+ *
+ * @author Bernd Eckenfels
+ */
+package net.eckenfels.test.jce;
