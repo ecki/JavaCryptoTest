ea6d534023541ce7306654a3c090e56c9ffdd98d
Bernd Eckenfels <bernd@eckenfels.net>
2013-11-14 21:40:50 +0100

    decribe classes

--- src/main/java/net/eckenfels/test/jce/package-info.java
+++ src/main/java/net/eckenfels/test/jce/package-info.java
@@ -1,5 +1,11 @@
 /**
  * Package to test some aspects of Java Cryptography API/Extension.
+ * <P>
+ * @link {@link net.eckenfels.test.jce.HashOverflowTest} is a Junit test case  based on a bug
+ * report (SHA-1 counter overflow in GnuPG/libcrypt) and used to verify the test vectors (for 257GB nul data)
+ * with JCE as well as BC. Note: runtime can be up to hours.
+ * <P>
+ * {@link net.eckenfels.test.jce.JCEProviderInfo} can be used to list the JCE providers and their capabilities in a JRE.
  *
  * @author Bernd Eckenfels
  */
