67ba80422e168b07116a086fab9fa9cb184d7615
Bernd Eckenfels <bernd@eckenfels.net>
2014-02-16 00:54:35 +0100

    Some formatting to the output, some more system and session information.

--- src/main/java/net/eckenfels/test/howsmyssl/Client.java
+++ src/main/java/net/eckenfels/test/howsmyssl/Client.java
@@ -16,10 +16,16 @@ public class Client
 {
     public static void main(String[] args) throws UnsupportedEncodingException, IOException
     {
-        System.out.println("Howsmyssl Test: " + System.getProperty("java.runtime.version")+"/"+System.getProperty("java.vm.version") + " " + System.getProperty("java.vm.name"));
+        System.out.printf("Howsmyssl Test: %s %s/%s on %s %s%n", System.getProperty("java.vm.name"),
+                          System.getProperty("java.runtime.version"),
+                          System.getProperty("java.vm.version"),
+                          System.getProperty("os.name"),
+                          System.getProperty("os.version"));
+
         SSLSocketFactory sf = (SSLSocketFactory)SSLSocketFactory.getDefault();
         SSLSocket s = (SSLSocket)sf.createSocket("www.howsmyssl.com", 443);
         s.startHandshake();
+        System.out.printf("Cipher used %s %s%n", s.getSession().getProtocol(), s.getSession().getCipherSuite());
         Thread printer = new InputStreamPrinterThread(s.getInputStream());
         OutputStream out = s.getOutputStream();
         out.write("GET https://www.howsmyssl.com/a/check HTTP/1.1\n\rHost: www.howsmyssl.com\n\r\n\r".getBytes("ISO-8859-1"));
@@ -43,7 +49,11 @@ public class Client
                 BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
                 String line;
                 while ((line = reader.readLine()) != null)
+                {
+                    // very sophisticated json formatter:
+                    line = line.replaceAll(",\"", ",\n  \"");
                     System.out.println(line);
+                }
             }
             catch (Exception ex) { throw new RuntimeException(ex); }
             finally { try { in.close(); } catch (Exception ignored) { } }
